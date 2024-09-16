76f4d70cdcbaca7831f060ace411da6d9967682c
LesegoLekwadi <lesegolekwadi8@gmail.com>
2024-09-16 15:56:29 +0200

    modification

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,58 +1,96 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Icon } from '@iconify/react';
-import experienceIcon from '@iconify/icons-logos/react'; // Example icon
-import './Experience.css'; // Import the CSS file
+import React, { Component } from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import Badge from "react-bootstrap/Badge";
 
-// Sample data for experiences
-const experiences = [
-  {
-    title: "Full-Stack",
-    company: "Shaper",
-    duration: "2024",
-    description: "Developing and maintaining web applications using modern technologies.",
-    icon: experienceIcon,
-  },
-  {
-    title: "Frontend Developer",
-    company: "Another Tech Company",
-    duration: "Jun 2020 - Dec 2021",
-    description: "Worked on building responsive and user-friendly interfaces.",
-    icon: experienceIcon,
-  },
-];
+class Experience extends Component {
+  render() {
+    if (this.props.resumeExperience && this.props.resumeBasicInfo) {
+      var sectionName = this.props.resumeBasicInfo.section_name.experience;
+      var work = this.props.resumeExperience.map(function (work, i) {
+        const technologies = work.technologies;
+        const mainTechnologies = work.mainTech;
 
-const Experience = () => {
-  return (
-    <section id="experience">
-      <h2>Experience</h2>
-      <div className="experience-list">
-        {experiences.map((exp, index) => (
-          <div key={index} className="experience-item">
-            <Icon icon={exp.icon} className="experience-icon" />
-            <div>
-              <h3>{exp.title}</h3>
-              <p><strong>{exp.company}</strong></p>
-              <p>{exp.duration}</p>
-              <p>{exp.description}</p>
+        var mainTech = mainTechnologies.map((technology, i) => {
+          return (
+            <Badge pill className="main-badge mr-2 mb-2" key={i}>
+              {technology}
+            </Badge>
+          );
+        });
+        var tech = technologies.map((technology, i) => {
+          return (
+            <Badge pill className="experience-badge mr-2 mb-2" key={i}>
+              {technology}
+            </Badge>
+          );
+        });
+        return (
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date={work.years}
+            iconStyle={{
+              background: "#AE944F",
+              color: "#fff",
+              textAlign: "center",
+            }}
+            icon={<i className="fab fa-angular experience-icon"></i>}
+            key={i}
+          >
+            <div style={{ textAlign: "left", marginBottom: "4px" }}>
+              {mainTech}
             </div>
-          </div>
-        ))}
-      </div>
-    </section>
-  );
-};
 
-Experience.propTypes = {
-  experiences: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      company: PropTypes.string.isRequired,
-      duration: PropTypes.string.isRequired,
-      description: PropTypes.string.isRequired,
-      icon: PropTypes.object.isRequired,
-    })
-  ),
-};
+            <h3
+              className="vertical-timeline-element-title"
+              style={{ textAlign: "left" }}
+            >
+              {work.title}
+            </h3>
+            <h4
+              className="vertical-timeline-element-subtitle"
+              style={{ textAlign: "left" }}
+            >
+              {work.company}
+            </h4>
+            <div style={{ textAlign: "left", marginTop: "15px" }}>{tech}</div>
+          </VerticalTimelineElement>
+        );
+      });
+    }
+
+    return (
+      <section id="resume" className="pb-5">
+        <div className="col-md-12 mx-auto">
+          <div className="col-md-12">
+            <h1 className="section-title" style={{ color: "black" }}>
+              <span className="text-black" style={{ textAlign: "center" }}>
+                {sectionName}
+              </span>
+            </h1>
+          </div>
+        </div>
+        <div className="col-md-8 mx-auto">
+          <VerticalTimeline>
+            {work}
+            <VerticalTimelineElement
+              iconStyle={{
+                background: "#AE944F",
+                color: "#fff",
+                textAlign: "center",
+              }}
+              icon={
+                <i className="fas fa-hourglass-start mx-auto experience-icon"></i>
+              }
+            />
+          </VerticalTimeline>
+        </div>
+      </section>
+    );
+  }
+}
 
 export default Experience;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,49 +1,64 @@
-import React from 'react';
-import './Project.css'; // Import the CSS file
+import React, { Component } from "react";
 
-const projects = [
-  {
-    id: 1,
-    title: "Project One",
-    description: "A detailed description of Project One with more information.",
-    link: "https://example.com/project-one",
-    technologies: ["React", "Node.js"],
-    image: "https://corporate-assets.lucid.co/chart/09255df0-f147-42b4-805e-163ad3001feb.png?v=1707845547429",
-  },
-  {
-    id: 2,
-    title: "Project Two",
-    description: "A detailed description of Project Two with more information.",
-    link: "https://example.com/project-two",
-    technologies: ["Angular", "Node.js"],
-    image: "https://www.liquidplanner.com/wp-content/uploads/2019/04/HiRes-17.jpg",
-  },
-  // Add more projects as needed
-];
 
-const Project = () => {
-  return (
-    <section id="projects">
-      <h2>Projects</h2>
-      <div className="project-list">
-        {projects.map((project) => (
-          <div key={project.id} className="project-item">
-            <img src={project.image} alt={project.title} className="project-image" />
-            <h3>{project.title}</h3>
-            <p>{project.description}</p>
-            <ul className="technologies">
-              {project.technologies.map((tech, index) => (
-                <li key={index}>{tech}</li>
-              ))}
-            </ul>
-            <a href={project.link} target="_blank" rel="noopener noreferrer">
-              View Project
-            </a>
+class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deps: {},
+      
+    };
+  }
+
+  render() {
+ 
+
+  
+    if (this.props.resumeProjects && this.props.resumeBasicInfo) {
+      var sectionName = this.props.resumeBasicInfo.section_name.projects;
+      var projects = this.props.resumeProjects.map(function (projects) {
+        return (
+          <div
+            className="col-sm-12 col-md-6 col-lg-4"
+            key={projects.title}
+            style={{ cursor: "pointer" }}
+          >
+            <span className="portfolio-item d-block">
+              
+                <div>
+                  <img
+                    src={projects.images[0]}
+                    alt="projectImages"
+                    height="230"
+                    style={{marginBottom: 0, paddingBottom: 0, position: 'relative'}}
+                  />
+                  <span className="project-date">{projects.startDate}</span>
+                  <br />
+                  <p className="project-title-settings mt-3">
+                    {projects.title}
+                  </p>
+                </div>
+              
+            </span>
           </div>
-        ))}
-      </div>
-    </section>
-  );
-};
+        );
+      });
+    }
 
-export default Project;
+    return (
+      <section id="portfolio">
+        <div className="col-md-12">
+          <h1 className="section-title" style={{ color: "black" }}>
+            <span>{sectionName}</span>
+          </h1>
+          <div className="col-md-12 mx-auto">
+            <div className="row mx-auto">{projects}</div>
+          </div>
+       
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Projects;
